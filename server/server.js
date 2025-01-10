cf21e0d9008c7adfaf791c942a125e8a587000b8
Andre Augustin <91437369+AAugusitn2542@users.noreply.github.com>
2025-01-10 12:26:55 -0800

    Update server.js and deleted the keep alive

--- server/server.js
+++ server/server.js
@@ -76,6 +76,4 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-server.keepAliveTimeout = 120000; // 2 minutes
-server.headersTimeout = 120000; // 2 minutes
 
