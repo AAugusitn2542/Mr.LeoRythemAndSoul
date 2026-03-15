e5773de866e2efb11eb0550151ddeecd87f83827
AAugusitn2542 <augustinandre010@gmail.com>
2026-03-15 13:42:16 -0700

    updating thumbnail

--- data/performance.js
+++ data/performance.js
@@ -5,7 +5,7 @@ export const performanceData = [
     location: "Los Gatos, CA",
     date: "2026-01-10",
     videoSrc: "/video/cats02.mp4",
-    thumbnail: "/images/fbgrouppic02.jpg",
+    thumbnail: "/images/gallerypic05.png",
   },
   {
    id: 2,
@@ -21,15 +21,15 @@ export const performanceData = [
     location: "Los Altos, CA",
     date: "2025-12-04",
     videoSrc: "/video/cats04.mp4",
-    thumbnail: "/images/fbgrouppic02.jpg",
+    thumbnail: "/images/bandsing.jpg",
   },
   {
     id: 4,
-    title: "Live at The Cats",
-    location: "Los Gatos, CA",
-    date: "2025-08-15",
+    title: "Live at Stilettos & Sneakers",
+    location: "San Jose, CA",
+    date: "2026-03-14",
     videoSrc: "/video/sanjose.mp4",
-    thumbnail: "/images/gallerypic07.png",
+    thumbnail: "/images/gallerypic06.png",
   },
   
 ];
